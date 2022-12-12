108c89ed0c27b2e0c7fbd1e5b461d8d45fe20500
Et3rez <gene13th@gmail.com>
2022-12-12 21:58:09 +0100

    add chart generating

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+let years = [];
+let population = [];
+
 // wyszukiwanie panstwa
 $("#search-country").submit(function(e){
     e.preventDefault();
 
     $("#boxWykresPopulacja").css({"display":"none"});
     let country = $("#country").val();
-    console.log(country)
-
+    
     if(country.length > 0) {
         //console.log(country);
         //zapytanie do API
@@ -25,6 +27,26 @@ $("#search-country").submit(function(e){
                 $("#wykresDla").html(response.data.country);
                 $("#lataOd").html(response.data.populationCounts[0].year);
                 $("#lataDo").html(response.data.populationCounts[response.data.populationCounts.length-1].year);
+
+                let populationWithCountry = [];
+
+                response.data.populationCounts.forEach(rok => {
+                    //console.log(`${rok.year} ${rok.value}`);
+                    years.push(rok.year);
+                    populationWithCountry.push(rok.value);
+
+                })
+
+                population.push({
+                    country: country,
+                    population: populationWithCountry
+                })
+
+                // generowanie wykresu
+                generowanieWykresuPopulacji(years, population);
+            },
+            error: function() {
+                alert(`Brak danych dla: ${country}`);
             }
         })
     }
@@ -32,3 +54,27 @@ $("#search-country").submit(function(e){
         alert("Wprowadź nazwę państwa!");
     }
 })
+
+function generowanieWykresuPopulacji(lata, populacja) {
+    $("#wykresPopulacji").html('<canvas id="wykresCanvasPopulacji" width="100%" height="300"></canvas>')
+
+    let data = [];
+
+    populacja.forEach(pop => {
+        data.push({
+            data: pop.population,
+            label: pop.country
+        })
+    })
+
+    console.log(data);
+
+
+    new Chart($("#wykresCanvasPopulacji"), {
+        type:"line",
+        data: {
+            labels: lata,
+            datasets: data
+        }
+    })
+}
