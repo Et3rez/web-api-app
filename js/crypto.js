bcbdda412ccd9d0113f5f38c7cd01be45da2aac1
Et3rez <90441928+Et3rez@users.noreply.github.com>
2023-01-04 21:17:16 +0100

    add: input form reading

--- js/crypto.js
+++ js/crypto.js
@@ -1,1 +1,24 @@
-console.log('krypto test');
+// funkcja wyszukania kryptowaluty
+$("#search-crypto").submit(function(e){
+    e.preventDefault();
+
+    let crypto = $("#crypto").val();
+    let days = $("#days").val();
+
+    if(crypto.length > 0){
+        if(days<0 || days=="" || days>365){
+            days = 7;
+        }
+
+        // zapytanie o kursy walut
+        // tutaj....
+        console.log(`waluta: ${crypto}`);
+        console.log(`dni: ${days}`);
+
+    }
+    else{
+        alert("Uzupełnij wszystkie pola formularza.");
+    }
+
+    console.log(`${crypto} ${days}`);
+})
